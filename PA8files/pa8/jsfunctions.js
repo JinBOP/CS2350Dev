74965779b100bd5c9f78fdcb8cde9f8476b83002
Jin <novhunter@gmail.com>
2023-11-02 13:17:28 -0600

    implemented sphere volume calculator

--- PA8files/pa8/jsfunctions.js
+++ PA8files/pa8/jsfunctions.js
@@ -3,7 +3,9 @@ function constructmenu() {
     main = document.getElementById("maindish").value;
     side1 = document.getElementById("sidedishone").value;
     side2 = document.getElementById("sidedishtwo").value;
-    document.getElementById("menu").innerHTML = "The menu for dinner tonight includes " + main + ", " + side1 + ", and " + side2 + "."
+    document.getElementById("menu").innerHTML = 
+    "The menu for dinner tonight includes " + main + ", " + side1 + 
+    ", and " + side2 + "."
 }
 
 function computeaverage() {
@@ -12,5 +14,16 @@ function computeaverage() {
     quiz2 = document.getElementById("quiztwo").value;
     quiz3 = document.getElementById("quizthree").value;
     average = (parseFloat(quiz1) + parseFloat(quiz2) + parseFloat(quiz3)) / 3;
-    document.getElementById("average").innerHTML = "The average score is " + average.toFixed(1) + "."; 
+    document.getElementById("average").innerHTML = 
+    "The average score is " + average.toFixed(1) + "."; 
+}
+
+function computevolume() {
+    var radius, volume;
+    radius = document.getElementById("radius").value;
+    volume = 4 / 3 * 3.14159 * 
+    parseFloat(radius) * parseFloat(radius) * parseFloat(radius);
+    document.getElementById("volume").innerHTML = 
+    "The volume of a sphere with radius " + 
+    radius + " is " + volume.toFixed(2) + ".";
 }
