e12685628facd15cd139e468cfdf544b81912aa9
Jin <novhunter@gmail.com>
2023-11-02 13:10:29 -0600

    added average calculator

--- PA8files/pa8/jsfunctions.js
+++ PA8files/pa8/jsfunctions.js
@@ -5,3 +5,12 @@ function constructmenu() {
     side2 = document.getElementById("sidedishtwo").value;
     document.getElementById("menu").innerHTML = "The menu for dinner tonight includes " + main + ", " + side1 + ", and " + side2 + "."
 }
+
+function computeaverage() {
+    var quiz1, quiz2, quiz3, average;
+    quiz1 = document.getElementById("quizone").value;
+    quiz2 = document.getElementById("quiztwo").value;
+    quiz3 = document.getElementById("quizthree").value;
+    average = (parseFloat(quiz1) + parseFloat(quiz2) + parseFloat(quiz3)) / 3;
+    document.getElementById("average").innerHTML = "The average score is " + average.toFixed(1) + "."; 
+}
