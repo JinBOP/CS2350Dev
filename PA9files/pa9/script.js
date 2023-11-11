7fbb2806a1c0662869a36f319d03d61209c085db
Jin <novhunter@gmail.com>
2023-11-11 14:42:04 -0700

    added play button to cycle through pics on a timer

--- PA9files/pa9/script.js
+++ PA9files/pa9/script.js
@@ -2,9 +2,13 @@ window.addEventListener("load", AddListeners);
 var images = new Array("alligator.jpg", "beware.jpg", "fatBoy.jpg", "gatorEyes.jpg", "gatorGoing.jpg", "gatorRain.jpg");
 var captions = new Array("gator boy", "watch out!", "fatty", "peepers", "on the move", "in the rain");
 var index = 0;
+var timer;
 
 function AddListeners() {
     document.getElementById("next").addEventListener("click", ChangeImage);
+    document.getElementById("play").addEventListener("click", function(){ 
+        timer = setInterval("ChangeImage()",1000);
+    });
 }
 
 function ChangeImage() {
