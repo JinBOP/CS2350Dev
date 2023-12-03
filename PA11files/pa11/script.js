3404e4ef458f26b2227340e662b432754e04ea06
Jin <novhunter@gmail.com>
2023-12-03 10:56:07 -0700

    added event handler for days boarding

--- PA11files/pa11/script.js
+++ PA11files/pa11/script.js
@@ -18,10 +18,27 @@ function addListeners() {
         else if (weight > 12 && weight <= 50){
             size = "medium";
         }
-        else if (weight > 50){
+        else{
             size = "large";
         }
 
         document.getElementById("size").value = size;
+    })
+
+    document.getElementById("days").addEventListener("change", function(){
+        var days = document.getElementById("days").value;
+        days = parseInt(days);
+        var fee;
+
+        if (isNaN(days)){
+            days = 0;
+            document.getElementById("boardingFee").value = "0.00";
+        }
+        else{
+            fee = 19.99 * days;
+        }
+
+        document.getElementById("days").value = days;
+        document.getElementById("boardingFee").value = fee.toFixed(2);
     })
 }
