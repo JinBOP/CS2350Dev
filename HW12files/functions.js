189604528ada40eeb3f5fbc42745891eebf1e491
Jin <novhunter@gmail.com>
2023-12-10 14:06:36 -0700

    added animations

--- HW12files/functions.js
+++ HW12files/functions.js
@@ -1,74 +1,30 @@
 $(document).ready(function(){   // selector runs when webpage is fully loaded
+    $('.silouette').hide();
+    $('.name').hide();
     $('.backdrop').fadeOut(1, function(){
-        $('.backdrop').delay(1000).fadeIn(1000)
+        $('.backdrop').delay(1000).fadeIn(1000, function(){
+            $('.silouette').show()
+            $('.silouette').animate({
+                height:'450px',
+                width:'450px',
+                top:'200px',
+                left:'200px'
+            }, 600 
+            );
+        });
     });
-    // $('.sue').slideUp(1000);
-    // $('.sue').delay(1000).slideDown(1000, function(){
-    //     $('div').fadeOut(2000);
-    //     $('div').delay(1000).fadeIn(2000);
-    // });
 
+    $('#reveal').click(function(){   // occurs when user clicks reveal button
+        $('.silouette').fadeOut();
 
-//     $('#fireworks').click(function(){   // occurs when user clicks fireworks button
-//         $('h2').fadeOut();
-
-//         // resets animation
-//         $('.purple').animate({
-//             height:'0px',
-//             width:'0px',
-//             top:'300px',
-//             left:'460px'
-//         },function(){
-//             $(this).show();
-//         });
-
-//         $('.green').animate({
-//             height:'0px',
-//             width:'0px',
-//             top:'300px',
-//             left:'460px'
-//         }, function(){
-//             $(this).show();
-//         });
-
-//         $('.red').animate({
-//             height:'0px',
-//             width:'0px',
-//             top:'300px',
-//             left:'460px'
-//         }, function(){
-//             $(this).show();
-//         });
-
-//         // carries out animation
-//         $('.purple').animate({
-//             height:'640px',
-//             width:'640px',
-//             top:'15px',
-//             left:'150px'
-//         }, 600, 'linear', function(){
-//             $(this).hide();
-//         });
-
-//         $('.green').delay(560).animate({
-//             height:'640px',
-//             width:'640px',
-//             top:'15px',
-//             left:'150px'
-//         }, 600, 'linear', function(){
-//             $(this).hide();
-//         });
-
-//         $('.red').delay(1100).animate({
-//             height:'640px',
-//             width:'640px',
-//             top:'15px',
-//             left:'150px'
-//         }, 600, 'linear', function(){
-//             $(this).fadeOut(600);
-//         });
-
-//         $('h2').delay(2200).fadeIn();
-
-//     });
+        $('.answer').show()
+        $('.name').show()
+            $('.answer').animate({
+                height:'450px',
+                width:'450px',
+                top:'200px',
+                left:'200px'
+            }, 600 
+            );
+    });
 })
